288d3a105f1f80cdabaa10910bd01e40ae594e6d
thanhlong160100@outlook.com.vn <thanhlong160100@outlook.com.vn>
2022-06-15 22:20:44 +0700

    Add Controller AdminAccounts, Class_Course, Class_Test, Course, Grades Add project files.

---
+++ E-Learning/Entity/Grade.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_Learning.Entity
+{
+    [Table("Grade")]
+    public class Grade
+    {
+
+        public Grade()
+        {
+            Classes = new HashSet<Class>();
+        }
+
+        public int gradeId { get; set; }
+
+        [StringLength(3)]
+        public string gradeName { get; set; }
+
+        public bool? Status { get; set; }
+
+
+        public ICollection<Class> Classes { get; set; }
+    }
+}
---
+++ E-Learning/Entity/Position.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_Learning.Entity
+{
+    [Table("Position")]
+    public class Position
+    {
+
+        public Position()
+        {
+            Admins = new HashSet<Admin>();
+        }
+
+        public int positionId { get; set; }
+
+        [StringLength(30)]
+        public string positionName { get; set; }
+
+        public bool? Status { get; set; }
+
+
+        public ICollection<Admin> Admins { get; set; }
+    }
+}
